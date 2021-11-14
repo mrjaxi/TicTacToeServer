281fd806ce3e481add9389b6f7d3386f9df90a83
BumbleBeeLion <roma16769@gmail.com>
2021-11-15 04:28:22 +0500

    Мелкие поправки

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -30,31 +30,31 @@ class ApiController extends AbstractController
     #[Route('/api/method.{name<\w+>}', name: 'api')]
     public function api(string $name, Request $request): JsonResponse
     {
-        if ($name == "getUserMove"){
-            $api_request['userMove'] = array(
-                'User1' => true,
-                'User2' => false,
-                'gameToken' => $request->query->get('game_token'),
-                'userPassword' => $this->encoder->encodePassword(new User(), "Password"),
-            );
-            } else if ($name == "loginUser"){
-            $userLogin = $request->query->get('userLogin');
-            $userPassword = $request->query->get('userPassword');
+        switch ($name) {
+            case "getUserMove":
+                $api_request['userMove'] = array(
+                    'User1' => true,
+                    'User2' => false,
+                    'gameToken' => $request->query->get('game_token'),
+                    'userPassword' => $this->encoder->encodePassword(new User(), "Password"),
+                );
+                break;
+            case "loginUser":
+                $userLogin = $request->query->get('userLogin');
+                $userPassword = $request->query->get('userPassword');
 
-            $returnValue = $this->usersService->createUser($userLogin, $userPassword);
+                $returnValue = $this->usersService->createUser($userLogin, $userPassword);
 
-            $api_request['response'] = array(
-                "userName" => $userLogin,
-                "token" => $returnValue
-            );
-        } else if ($name == "getUsers"){
-            $api_request['response'] = $this->usersService->getUsers();
+                $api_request['response'] = array(
+                    "userName" => $userLogin,
+                    "token" => $returnValue
+                );
+                break;
+            case "getUsers":
+                $api_request['response'] = $this->usersService->getUsers();
+                break;
         }
 
         return $this->json($api_request);
-
-        // return $this->render('api/index.html.twig', [
-        //     'json_api' => $api_request,
-        // ]);
     }
 }
