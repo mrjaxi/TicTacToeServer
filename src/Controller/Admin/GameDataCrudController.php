0ac3bdc641eb27b55d5ad5326781688227da185d
BumbleBeeLion <roma16769@gmail.com>
2021-11-29 18:56:26 +0500

    Поправил отображение GameData в админ панели

--- src/Controller/Admin/GameDataCrudController.php
+++ src/Controller/Admin/GameDataCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\GameData\Model\GameData;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -21,6 +22,8 @@ class GameDataCrudController extends AbstractCrudController
             IdField::new('matchid')
                 ->hideWhenCreating(),
             IdField::new('userId'),
+            BooleanField::new('bot'),
+            BooleanField::new('winner'),
             TextField::new('left_state'),
             TextField::new('right_state'),
             ArrayField::new('images_id'),
